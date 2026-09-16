765f3fd2be86f10308ceb42239c17a19b8c75e29
John Doe <johndoe@example.com>
2026-09-16 14:20:52 +0300

    commit test

--- stages/01-backend/src/handlers.ts
+++ stages/01-backend/src/handlers.ts
@@ -1,7 +1,7 @@
 import type { FastifyInstance } from 'fastify';
 import type { TicketService } from './service.ts';
 
-/** TODO */
+/** TODO 1231313131 */
 export async function registerTicketHandlers(_app: FastifyInstance, _service: TicketService): Promise<void> {
   throw new Error('TODO: implement registerTicketHandlers');
 }
